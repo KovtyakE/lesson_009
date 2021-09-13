c32c76e5dff131764fc327c1f7b74fb32d3f2137
EVG <evgeniy-kovtyak@mail.ru>
2021-09-13 12:57:41 +0300

    first half of char stat is ready. working. need to add different sorties

--- 01_char_stat.py
+++ 01_char_stat.py
@@ -23,6 +23,66 @@
 
 # TODO здесь ваш код
 
+import zipfile
+
+
+class Statistics_for_file:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+        self.sorted_stat = {}
+
+    def unzip(self):
+        zip_file = zipfile.ZipFile(self.file_name, "r")
+        for filename in zip_file.namelist():
+            zip_file.extract(filename)
+        self.file_name = filename
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(file=self.file_name, mode='r', encoding='cp1251') as file:
+            for line in file:
+                self.collect_from_line(line=line[:-1])
+
+    def collect_from_line(self, line):
+        for char in line:
+            if char.isalpha():
+                if char in self.stat:
+                    self.stat[char] += 1
+                else:
+                    self.stat[char] = 1
+            else:
+                continue
+
+    def sort_by_count(self):
+        self.sorted_dict = sorted(self.stat, key=self.stat.get)
+        for key in self.sorted_dict:
+            self.sorted_stat[key] = self.stat[key]
+
+    # todo
+    # способ отсортировать в одном методе?
+    def sort_by_count_reversed(self):
+        pass
+
+    def sort_by_name(self):
+        pass
+
+    def print_sorted_stat(self):
+        print('{char:-<6}+{stat:->8}'.format(char='+', stat='+'))
+        print('|{char:^5}|{stat:>7}|'.format(char='Буква', stat='Частота'))
+        print('{char:-<6}+{stat:->8}'.format(char='+', stat='+'))
+        for key in self.sorted_stat:
+            print('|{key:^5}|{value:>7}|'.format(key=key, value=self.stat[key]))
+
+
+file_stat = Statistics_for_file(file_name='python_snippets/voyna-i-mir.txt.zip')
+file_stat.unzip()
+file_stat.collect()
+file_stat.sort_by_count()
+file_stat.print_sorted_stat()
+
+
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
